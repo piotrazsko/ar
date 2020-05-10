7f157b076ebbb0aa938c6289601619e3c31f8271
Serj <serj432@yandex.ru>
2020-05-10 11:40:21 +0300

    added girl

--- js/index.js
+++ js/index.js
@@ -3,12 +3,12 @@ window.onload = () => {
   button.innerText = "﹖";
   navigator.geolocation.getCurrentPosition(success);
   function success(position) {
-    const coordinates = document.querySelector('.coordinates')
+    const coordinates = document.querySelector(".coordinates");
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    coordinates.textContent = `lat:${latitude}, long:${longitude}`
+    coordinates.textContent = `lat:${latitude}, long:${longitude}`;
     let places = staticLoadPlaces();
-    renderPlaces(places,latitude,longitude);
+    renderPlaces(places, latitude, longitude);
     function staticLoadPlaces() {
       return [
         {
@@ -29,10 +29,17 @@ var models = [
     scale: "0.5 0.5 0.5",
     info: "Head, Lv. 5, HP 10/10",
     rotation: "0 180 0",
-  },{
+  },
+  {
     url: "./magnemite/scene.gltf",
     scale: "0.5 0.5 0.5",
     info: "Magnemite, Lv. 5, HP 10/10",
+    rotation: "0 180 0",
+  },
+  {
+    url: "./girl/scene.gltf",
+    scale: "0.5 0.5 0.5",
+    info: "Girl, Lv. 5, HP 10/10",
     rotation: "0 180 0",
   },
   {
@@ -69,7 +76,7 @@ var setModel = function (model, entity) {
   div.innerText = model.info;
 };
 
-function renderPlaces(places,latitude,longitude) {
+function renderPlaces(places, latitude, longitude) {
   let scene = document.querySelector("a-scene");
   places.forEach((place) => {
     let model = document.createElement("a-entity");
@@ -89,7 +96,7 @@ function renderPlaces(places,latitude,longitude) {
         modelIndex++;
         var newIndex = modelIndex % models.length;
         setModel(models[newIndex], entity);
-      console.log('index',newIndex)
+        console.log("index", newIndex);
       });
 
     scene.appendChild(model);
