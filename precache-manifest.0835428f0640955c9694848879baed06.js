5f208a2d1ecdd3274e181a46bbf24def5de526be
Petrashka <piotrazsko@gmail.com>
2020-05-10 18:32:21 +0300

    changed build process

---
+++ precache-manifest.0835428f0640955c9694848879baed06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2640ae752918fe0836ebcf50678a3c96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82bb6d35f3060236c025",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82bb6d35f3060236c025",
+    "url": "/static/js/main.2002201a.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "860cdbe7530d493dfaa465a7f04ed03b",
+    "url": "/static/media/scene.860cdbe7.gltf"
+  }
+]);
